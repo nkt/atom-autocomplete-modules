a329df4a74f2fd2fdb53440c6914a4bf6e5027ce
Nikita Gusakov <dev@nkt.me>
2015-10-23 10:10:39 +0300

    Changed code style config

--- src/completion-provider.js
+++ src/completion-provider.js
@@ -7,6 +7,8 @@ const fuzzaldrin = require('fuzzaldrin');
 const escapeRegExp = require('lodash.escaperegexp');
 const internalModules = require('./internal-modules');
 
+const MODULE_REGEXP = /require|import|export\s+(\*|{[a-zA-Z0-9_$,\s]+})+\s+from/;
+
 class CompletionProvider {
   constructor() {
     this.selector = '.source.js .string.quoted, .source.coffee .string.quoted';
@@ -16,7 +18,7 @@ class CompletionProvider {
 
   getSuggestions({editor, bufferPosition, prefix}) {
     const line = editor.getTextInRange([[bufferPosition.row, 0], bufferPosition]);
-    if (!/require|import|export\s+(\*|{[a-zA-Z0-9_$,\s]+})+\s+from/.test(line)) {
+    if (!MODULE_REGEXP.test(line)) {
       return [];
     }
 
@@ -80,7 +82,7 @@ class CompletionProvider {
     }
 
     const nodeModulesPath = path.join(projectPath, 'node_modules');
-    if (prefix.indexOf('/') !== -1) {
+    if (prefix.includes('/')) {
       return this.lookupLocal(`./${prefix}`, nodeModulesPath);
     }
 
