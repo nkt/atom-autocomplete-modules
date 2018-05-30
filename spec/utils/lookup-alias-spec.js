9419d4e0be88b63d14a6d91bf31296065d6252b2
Jonathan Yee <jonyeezs@gmail.com>
2018-05-30 22:18:00 +1000

    :truck: (refactor) refactor webpack and looking up alias

---
+++ spec/utils/lookup-alias-spec.js
@@ -0,0 +1,25 @@
+const subject = require('../../lib/utils/lookup-alias');
+const path = require('path');
+const basePath = path.resolve('../fixtures');
+
+const aliases = [{
+  expose: '@sub',
+  src: `${basePath}/subfolder`
+}, {
+  expose: '@library',
+  src: `${basePath}/node_modules/commonjs`
+}];
+
+describe('lookupAlias', () => {
+  it('should lookup subfolder alias', (done) => {
+    const prefix = '@sub/subfile';
+
+    subject(prefix, basePath, aliases).then((results) => {
+      expect(results.length).toBe(1);
+
+      expect(results[0].prefixWithoutAlias).toBe('subfile');
+      expect(results[0].pathToAlias).toBe(`${basePath}/subfolder`)
+      done();
+    });
+  });
+});
