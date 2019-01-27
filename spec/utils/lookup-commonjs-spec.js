0736781d908430c25c5ef0d62de49ae0f497ba64
Jonathan Yee <jonyeezs@gmail.com>
2019-01-27 23:19:04 +1000

    fix(require-lookup): Remove using require and use acorn AST instead
    Requiring a file was also executing files. that behaviour is a security issue
    
    #126, #100

--- spec/utils/lookup-commonjs-spec.js
+++ spec/utils/lookup-commonjs-spec.js
@@ -23,6 +23,13 @@ describe('lookupCommonjs', () => {
     });
   });
 
+  describe('multiple exports', () => {
+    it('should list all exports', () => {
+      const result = subject('./node_modules/package/file1', basePath);
+      expect(result).toHaveLength(4);
+    })
+  });
+
   it('should return no results if the file does not exist', () => {
     const result = subject('./noexist/this', `${basePath}/fake`);
     expect(result).toHaveLength(0);
